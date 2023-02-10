8064be365ce3186f4bd9b7fd73d7b1f91c738099
TalalMash <0talal.mash0@gmail.com>
2023-02-09 20:34:53 -0600

    Hide menu to unauth & rename

--- luasrc/controller/eqos.lua
+++ luasrc/controller/eqos.lua
@@ -1,11 +1,15 @@
 module("luci.controller.eqos", package.seeall)
 
+local fs = require "nixio.fs"
+local sys = require "luci.sys"
+local template = require "luci.template"
+local i18n = require "luci.i18n"
+
 function index()
-	if not nixio.fs.access("/etc/config/eqos") then
-		return
-	end
-	
-	local page
-        entry({"admin", "nas"}, firstchild(), "NAS", 45).dependent = false
-	entry({"admin", "network", "eqos"}, cbi("eqos"), _("EQoS"))
+        if not nixio.fs.access("/etc/config/eqos") then
+                return
+        end
+        local e = entry({"admin", "network", "eqos"}, cbi("eqos"), _("Client QoS"), 100)
+        e.acl_depends = { "luci-app-eqos" }
+        e.dependent = true
 end
